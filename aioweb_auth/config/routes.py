7269c2c235a3ef5cc41db2b360e2c141fc8874b5
kreopt <kreopt@gmail.com>
2017-06-28 14:40:33 +0700

    * delete method for logout

--- aioweb_auth/config/routes.py
+++ aioweb_auth/config/routes.py
@@ -2,3 +2,4 @@ def setup(router):
     router.root('auth#index')
     router.post('/', 'auth#login', name='do_login')
     router.post('/logout', 'auth#logout', name='logout')
+    router.delete('/', 'auth#logout', name='logout_')
