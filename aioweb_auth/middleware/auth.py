1808472bbc527010e9e407b4aac195fb0ce25578
kreopt <kreopt@gmail.com>
2017-07-09 12:27:46 +0700

    * do not set csrf token manually

--- aioweb_auth/middleware/auth.py
+++ aioweb_auth/middleware/auth.py
@@ -27,8 +27,6 @@ async def process_auth(request, response):
     if request.get(REQUEST_KEY):
         if request[REQUEST_KEY].get('remember') and request.user.is_authenticated():
             await remember(request, response, request.user.username)
-            session = await get_session(request)
-            session[CSRF_SESSION_NAME] = request.csrf_token
         if request[REQUEST_KEY].get('forget'):
             await forget(request, response)
             session = await get_session(request)
